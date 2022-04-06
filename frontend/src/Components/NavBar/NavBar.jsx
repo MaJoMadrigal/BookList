4e7f674de0be7e0f4903df3e15debe06ec62dd85
MaJoMadrigal <madrigal.mj.2@pg.com>
2022-04-06 15:45:46 -0600

    Add newBook button style

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -8,7 +8,20 @@ function NavBar() {
       <Navbar.Brand href='#home'>
         <img alt='' src={icon} width='80' height='70' /> Book List App
       </Navbar.Brand>
+      <Button className=""></Button>
     </Navbar>
   )
 }
 export default NavBar
+
+
+/* .addBookBtn{
+    background-color: transparent;
+    border-color: transparent;
+    color: rgba(255,255,255,.55);
+}
+.addBookBtn:hover{
+    color: white;
+    border-color: #00CED1;
+    background-color: transparent;
+} */
